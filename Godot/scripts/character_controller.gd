b6bbd282fd31dd1acfd8170747eda3a2a3ce0ec3
Brandon Michelsen <brandongmichelsen@gmail.com>
2022-04-14 20:00:25 -0700

    Got player death outside camera working

--- Godot/scripts/character_controller.gd
+++ Godot/scripts/character_controller.gd
@@ -10,18 +10,27 @@ var velocity: Vector2 = Vector2()
 
 const FLOOR_NORMAL: Vector2 = Vector2(0, -1) # Direction of a floor
 
-onready var SCREEN_WIDTH: float = get_viewport_rect().size.x
+onready var SCREEN_WIDTH: float = get_viewport_rect().size.x	# Viewport width
+onready var SCREEN_HEIGHT: float = get_viewport_rect().size.y	# Viewport height
+
+var scrolling: bool
+var credits
+var camera
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	credits = get_node("/root/Game/CreditsLevel")
+	camera = credits.get_child(1)
+	camera.connect("scrolling", self, "_scrolling")
+	scrolling = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	get_input()								# Move based on input
 	velocity.y += GRAVITY * delta			# Apply accel. gravity
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)	# Move player
-	get_wrap()								# See if we need to wrap character around screen
+	chk_wrap()								# See if we need to wrap character around screen
+	chk_outside_scroll()
 
 	# See if we landed back on the floor
 	if jumping and is_on_floor():
@@ -48,7 +57,7 @@ func get_input():
 		velocity.y -= JUMP_VELOCITY
 
 # Called when checking if we need to wrap
-func get_wrap():
+func chk_wrap():
 	## NOTE: 	position is an inbuilt property 
 	## 			of the KinematicBody2D object
 
@@ -58,3 +67,26 @@ func get_wrap():
 	# Check if we are off left side of screen
 	if position.x < 0:
 		position.x = SCREEN_WIDTH-1
+
+# Called when checking if character is outside the scroll
+func chk_outside_scroll():
+	## NOTE:	position is an inbuilt property
+	##			of the KinematicBody2D object
+
+	# Get the current position of the scroll camera
+	var camera_y = camera.position.y
+	var upper = camera_y - (SCREEN_HEIGHT / 2)
+	var lower = camera_y + (SCREEN_HEIGHT / 2)
+
+	# Only check this if we are scrolling
+	if scrolling:
+		# Check if we are outside the camera
+		if (position.y < upper) or (position.y > lower):
+			print("dead")
+			get_tree().reload_current_scene()
+
+# Called when scrolling signal emitted from ScrollCamera
+func _scrolling():
+	print("scrolling")
+	scrolling = true
+	
--- Godot/scripts/scroll.gd
+++ Godot/scripts/scroll.gd
@@ -2,20 +2,38 @@ extends Camera2D
 
 ## Declare script variables
 
-export var MAX_SCROLL: float = 200
+export var MAX_SCROLL: float 	= 200	# Maximum scroll value (exported to editor)
+export var START_SCROLL: float	= 15	# Threshold to start scrolling (exported to editor)
+export var SCROLL_SPEED: float	= 1		# Speed to scroll at (exported to editor)
 
-var TILE_SIZE: float 	= 32
-var TOTAL_SCROLL: float
+var TILE_SIZE: float 	= 32			# Tile size (used to calculate scroll)
+var TOTAL_SCROLL: float					# The total scrolling value (MAX_SCROLL * TILE_SIZE)
+var SCROLLING_START: float				# Value to start scolling (START_SCROLL * TILE_SIZE)
+var SPEED: float						# Speed of scrolling (SCROLL_SPEED * TILE_SIZE)
+
+signal scrolling
+onready var scroll_emitted: bool = false
+
+# Reference to player object (to track it's position)
+onready var player = get_node("/root/Game/Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	make_current()	# Make this the main camera
 	print_debug(position.x)
 	TOTAL_SCROLL = MAX_SCROLL * TILE_SIZE
+	SCROLLING_START = START_SCROLL * TILE_SIZE
+	SPEED = SCROLL_SPEED * TILE_SIZE
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Make sure we stop scrolling at some point
-	if position.y < TOTAL_SCROLL:
-		position.y += 1
+	# Only start scrolling after the player has gone below
+	# a certain threshold
+	if player.position.y > SCROLLING_START:
+		if scroll_emitted == false:
+			emit_signal("scrolling")
+			scroll_emitted = true
+		# Make sure we stop scrolling at some point
+		if position.y < TOTAL_SCROLL:
+			position.y += SPEED
